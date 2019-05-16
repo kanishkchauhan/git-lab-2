e1ef06c8bc2f9f8a59158f15f3f6727495fb57b9
Kanishk Chauhan <kc303218@ohio.edu>
2019-05-16 08:19:07 +0530

    The First Hello Hit Program (Homework 1)

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Kanishk Chauhan
+ *        Date: May 15, 2019
+ * Description: lab program (homework 1)
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout<<"Hello Git!!"<<endl;
+    return 0;
+}// main
